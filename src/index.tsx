3b899d595655940586df732a798f1c05732d3610
YairTawil <yairtawil@gmail.com>
2018-06-05 10:43:54 +0300

    feat: router with redux (worlds, world state)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware, Reducer } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer as router, routerMiddleware, RouterState } from 'react-router-redux';
 import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import worlds, { IWorldsState } from './reducers/worlds';
+import './index.css'
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+export interface IState {
+    router: RouterState;
+    worlds: IWorldsState;
+}
+const reducers: Reducer<IState> = combineReducers<IState>({ router, worlds });
+
+const store = createStore(
+    reducers,
+    applyMiddleware(middleware)
+);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        { /* ConnectedRouter will use the store from Provider automatically */ }
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
 );
-registerServiceWorker();
+
